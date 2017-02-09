51c6e87ddaf149331c714079a5cae61e30438ab1
Zach <zachzhx@gmail.com>
2017-02-08 20:49:35 -0800

    Initial Commit

---
+++ WFAssignment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WFAssignment
+//
+//  Created by Zhang Xu on 2/8/17.
+//  Copyright © 2017 Zhang Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WFAssignment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WFAssignment
+//
+//  Created by Zhang Xu on 2/8/17.
+//  Copyright © 2017 Zhang Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
